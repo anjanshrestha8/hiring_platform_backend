f3a8d8a8bb79e751bc384b8f28512efdf15bcb7a
anjanshrestha8 <ajnstha2003@gmail.com>
2025-07-31 14:09:36 +0545

    find by pk for candiates is completed

--- controllers/candiateController.js
+++ controllers/candiateController.js
@@ -2,7 +2,7 @@ const Candidate = require('../models/Candidate');
 
 exports.createCandidate = async (request, response) => {
   try {
-    const candidate = Candidate.create(request.body);
+    const candidate = await Candidate.create(request.body);
     response.status(200).json({
       data: candidate,
       message: "Candiate is created sucessfully.",
@@ -12,18 +12,51 @@ exports.createCandidate = async (request, response) => {
       const errors = error.errors.map((e) => e.message);
       return res.status(400).json({ errors });
     }
-    res.status(500).json({ error: "Internal server error." });
+    response.status(500).json({ error: "Internal server error." });
   }
 };
 
 exports.getAllCandidates = async (request, response) => {
   try {
     const candidates = await Candidate.findAll();
+    if(candidates.length < 1){
+      response.status(200).json({
+        data: [],
+        message: "No candidates to display."
+      })
+    }
     response.status(200).json({
       data: candidates,
       message: "All candiates are displayed.",
     });
   } catch (error) {
-    res.status(500).json({ error: "Internal server error." });
+    response.status(500).json({ error: "Internal server error." });
   }
 };
+
+exports.getCandidatesById = async (request,response) => {
+  const id = request.params.id;
+
+  if(!id){
+    response.status(400).json({
+      data:[],
+      message: "Bad Request. Param is missing!!!!"
+    });
+  }
+  try {
+    const candidate = await Candidate.findByPk(id);
+    if(!candidate){
+       response.status(404).json({
+         data: [],
+         message: "Candidates not found!!!!",
+       });
+    }
+    response.status(200).json({
+      data: candidate,
+      message: "Candidate is found!!!!"
+    })
+    
+  } catch (error) {
+    response.status(500).json({ error: "Internal server error." });
+  }
+}
--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -4,5 +4,7 @@ const candidateController = require('../controllers/candiateController');
 
 router.post("/", candidateController.createCandidate);
 router.get("/", candidateController.getAllCandidates);
+router.get("/:id", candidateController.getCandidatesById);
+
 
 module.exports = router;
