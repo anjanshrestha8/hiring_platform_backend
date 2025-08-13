e6577110ec05477f734792c87019b6e753dde651
anjanshrestha8 <ajnstha2003@gmail.com>
2025-08-13 23:46:29 +0545

    cv uplaod

---
+++ middlewares/uploadCv.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+const path = require("path");
+
+// Storage configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/cvs"); // folder to store CVs
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+// File filter (only PDFs or DOCX)
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = [".pdf", ".doc", ".docx"];
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (allowedTypes.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF, DOC, and DOCX files are allowed"), false);
+  }
+};
+
+module.exports = multer({ storage, fileFilter });
